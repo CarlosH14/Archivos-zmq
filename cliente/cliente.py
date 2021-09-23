fdd6f3f336776c07c4f9cf80bdb2c1c63df84756
CarlosH14 <carlos.hincapie@utp.edu.co>
2021-09-23 18:23:39 -0500

    Cliente y servidor con segmentación

--- cliente/cliente.py
+++ cliente/cliente.py
@@ -11,44 +11,84 @@ if __name__ == "__main__":
     s = context.socket(zmq.REQ)
     s.connect('tcp://localhost:8001')
 
+    fin="fin"
+    fin=fin.encode('utf-8')
+
     nombre = sys.argv[1]
     funcion = sys.argv[2]
     aux = sys.argv[3]
+
+    auxseek0 = 0
+    auxseek10 = 1024*1024*10
+
     if funcion == "subir":
-        with open(aux, "rb") as file:
-            contenido = file.read()
+        file = open(aux, "rb")
+        file.seek(0)
+        nombre = nombre.encode('utf-8')
+        funcion = funcion.encode('utf-8')
+        aux = aux.encode('utf-8')
+        while True:
+            contenido = file.read(auxseek10)
+            if not contenido:
+                s.send_multipart([fin])
+                break
+            s.send_multipart([nombre, funcion, aux, contenido])
+            m = s.recv_multipart()
+            print(m[0], aux)
+
     if funcion == "descargar":
-        with open("vacio", "rb") as file:
-            contenido = file.read()
+        nombre = nombre.encode('utf-8')
+        funcion = funcion.encode('utf-8')
+        aux = aux.encode('utf-8')
+        while True:
+            contenido = b'0'
+            s.send_multipart([nombre, funcion, aux, contenido])
+            m = s.recv_multipart()
+            if m[0] == fin:
+                break
+            desc_aux= aux.decode('utf-8')
+            desc_cont= m[1]
+            with open(desc_aux,"wb") as file: #Abre el archivo solo para las funciones descargar y descargarenlace que retornan el nombre del archivo con su contenido
+                file.seek(auxseek0)
+                auxseek0 = auxseek0 + auxseek10
+
+                file.write(desc_cont)
+            m[1]=aux
+            print(m)
+
+
     if funcion == "descargarenlace":
-        with open("vacio", "rb") as file:
-            contenido = file.read()
+        nombre = nombre.encode('utf-8')
+        funcion = funcion.encode('utf-8')
+        aux = aux.encode('utf-8')
+        while True:
+            contenido = b'0'
+            s.send_multipart([nombre, funcion, aux, contenido])
+            m = s.recv_multipart()
+            if m[0] == fin:
+                break
+            enlace = aux.decode('utf-8') #Obtiene el enlace de la tercera posicion del comando y la recibe como parametro
+            ruta = enlace.split("$") #Separa en ruta el enlace antes y despues del $
+            n_arch = ruta[0].split("/") #Variable que guarda la dirección del archivo
+            ruta = (n_arch[2]) #Se crea la ruta a partir de los parametros anteriores
+            desc_cont= m[1]
+            with open(ruta,"wb") as file: #Abre el archivo solo para las funciones descargar y descargarenlace que retornan el nombre del archivo con su contenido
+                file.seek(auxseek0)
+                auxseek0 = auxseek0 + auxseek10
+
+                file.write(desc_cont)
+            m[1]=aux
+            print(m)
+
     if funcion == "enlace":
-        with open(aux, "rb") as file:
-            contenido = file.read()
-    nombre = nombre.encode('utf-8')
-    funcion = funcion.encode('utf-8')
-    aux = aux.encode('utf-8')
-
-
-    s.send_multipart([nombre, funcion, aux, contenido]) #Envia información en json
-
-    m = s.recv_multipart() #Recibe información en json
-
-    funcion=funcion.decode('utf-8')
-    if funcion == "descargar":
-        desc_aux= aux.decode('utf-8')
-        desc_cont= m[1]
-        with open(desc_aux,"wb") as file: #Abre el archivo solo para las funciones descargar y descargarenlace que retornan el nombre del archivo con su contenido
-            file.write(desc_cont)
-        m[1]=aux
-    if funcion == "descargarenlace":
-        enlace = aux.decode('utf-8') #Obtiene el enlace de la tercera posicion del comando y la recibe como parametro
-        ruta = enlace.split("$") #Separa en ruta el enlace antes y despues del $
-        n_arch = ruta[0].split("/") #Variable que guarda la dirección del archivo
-        ruta = (n_arch[2]) #Se crea la ruta a partir de los parametros anteriores
-        desc_cont= m[1]
-        with open(ruta,"wb") as file: #Abre el archivo solo para las funciones descargar y descargarenlace que retornan el nombre del archivo con su contenido
-            file.write(desc_cont)
-        m[1]=aux
-    print(m)
+        contenido = b'0'
+        nombre = nombre.encode('utf-8')
+        funcion = funcion.encode('utf-8')
+        aux = aux.encode('utf-8')
+        while True:
+            s.send_multipart([nombre, funcion, aux, contenido])
+            m = s.recv_multipart()
+            print("Enlace: " , m[0].decode('utf-8'))
+            if m[1] == fin:
+                s.send_multipart([fin])
+                break
--- servidor/servidor.py
+++ servidor/servidor.py
@@ -10,14 +10,7 @@ def carpeta_usuario(name:str) -> bool: #Funcion para determinar si una carpeta e
 
 #Funciones para los archivos que reciebn un numero de arguemntos con un nombre fijo
 
-def subir(nombre, funcion, aux, contenido): #Subir archivos
-    if not carpeta_usuario(nombre):
-        os.makedirs("archivos/{}".format(nombre)) #Crea una carpeta para el usuario si no la tiene
-    with open("archivos/{}/{}".format(nombre,aux),"wb") as file: #Abre el archivo
-        file.write(contenido) #Escribe en su contenido el contenido del argumento
-    respuesta = "subido"
-    respuesta.encode('utf-8')
-    return respuesta #Retorna el mensaje para el cliente
+
 
 def descargar(nombre, funcion, aux): #Descargar archivos
     ruta = "archivos/{}/{}".format(nombre,aux) #Elabora la ruta del archivo a partir del usuario y del nombre del archivo
@@ -29,16 +22,7 @@ def descargar(nombre, funcion, aux): #Descargar archivos
         contenido = file.read()
     return contenido
 
-def enlace(nombre, funcion, aux, contenido): #Función para obtener el enlace de un archivo
-    ruta = "archivos/{}/{}".format(nombre,aux) #Elabora una ruta del archivo a partir del usuario y del nombre del archivo
-    if not path.isfile(ruta): #Si no encuentra el archivo retorna un error
-        respuesta = "archivo no encontrado"
-        respuesta.encode('utf-8')
-        return respuesta
-    enlace = "enlace${}".format(ruta) #Crea un enlace para el archivo iniciando por "enlace$" y continuado por la ruta del archivo
-    print(enlace)
-    enlace.encode('utf-8')
-    return enlace #Retorna el mensaje para el cliente
+
 
 def descargarenlace(nombre, funcion, aux): #Función para descargar un archivo a partir de un enlace
     enlace = aux #Obtiene el enlace de la tercera posicion del comando y la recibe como parametro
@@ -67,37 +51,89 @@ if __name__ == "__main__":
     s = context.socket(zmq.REP)
     s.bind(ROUTE)
 
+    fin="fin"
+    fin=fin.encode('utf-8')
+
     while True:
         print('--- ESPERANDO SOLICITUD ---')
-        m = s.recv_multipart()  #Recibe el json con los parametros del comando
-        nombre=m[0]
-        nombre=nombre.decode('utf-8')
-        funcion=m[1]
-        funcion=funcion.decode('utf-8')
-        aux=m[2]
-        aux=aux.decode('utf-8')
-        if funcion == "subir":
-            contenido=m[3]
-        if funcion == "enlace":
-            contenido=m[3]
-        #Imprime en pantalla el nombre del usuario
-        print("Usuario: {}".format(nombre)) #Imprime en pantalla el nombre del usuario
-        print("Petición: {}".format(funcion)) #Imprime en pantalla la función ingresada
-        print("Auxiliar: {}".format(aux)) #Imprime en pantalla el nombre del usuario
-        if funcion == "subir":
-            r = subir(nombre, funcion, aux, contenido)
-            desc_cont = aux.encode('utf-8')
-        elif funcion == "enlace":
-            r = enlace(nombre, funcion, aux, contenido)
-            desc_cont = aux.encode('utf-8')
-        elif funcion == "descargar":
-            desc_cont = descargar(nombre, funcion, aux)
-            r="descargado por cliente"
-        elif funcion == "descargarenlace":
-            desc_cont = descargarenlace(nombre, funcion, aux)
-            r="descargado por enlace"
-        else: #Condición para función incorrecta
-            print("error")
-            r = "error"
-            desc_cont = aux.encode('utf-8')
-        s.send_multipart([r.encode('utf-8'), desc_cont]) #Envia la respuesta (retorno de la función correspondiente) en un json al cliente
+        auxseek0 = 0
+        auxseek10 = 1024*1024*10
+        while True:
+            m = s.recv_multipart()
+            if m[0] == fin:
+                s.send_multipart([fin])
+                break
+            nombre=m[0]
+            nombre=nombre.decode('utf-8')
+            funcion=m[1]
+            funcion=funcion.decode('utf-8')
+            aux=m[2]
+            aux=aux.decode('utf-8')
+            #Imprime en pantalla el nombre del usuario
+            print("Usuario: {}".format(nombre)) #Imprime en pantalla el nombre del usuario
+            print("Petición: {}".format(funcion)) #Imprime en pantalla la función ingresada
+            print("Auxiliar: {}".format(aux)) #Imprime en pantalla el nombre del usuario
+
+            if funcion == "subir":
+                contenido=m[3]
+                if not path.isdir("archivos/{}".format(nombre)):
+                    os.makedirs("archivos/{}".format(nombre)) #Crea una carpeta para el usuario si no la tiene
+                with open("archivos/{}/{}".format(nombre,aux),"wb") as file:
+                    file.seek(auxseek0)
+                    auxseek0 = auxseek0 + auxseek10
+                    file.write(contenido) #Escribe en su contenido el contenido del argumento
+                respuesta = "subido"
+                respuesta.encode('utf-8')
+
+            elif funcion == "enlace":
+                contenido=m[3]
+                ruta = "archivos/{}/{}".format(nombre,aux) #Elabora una ruta del archivo a partir del usuario y del nombre del archivo
+                if not path.isfile(ruta): #Si no encuentra el archivo retorna un error
+                    respuesta = "archivo no encontrado"
+                    respuesta.encode('utf-8')
+                enlace = "enlace${}".format(ruta) #Crea un enlace para el archivo iniciando por "enlace$" y continuado por la ruta del archivo
+                print(enlace)
+                enlace.encode('utf-8')
+                respuesta=enlace
+                desc_cont=fin
+
+
+            elif funcion == "descargar":
+                ruta = "archivos/{}/{}".format(nombre,aux) #Elabora la ruta del archivo a partir del usuario y del nombre del archivo
+                if not path.isfile(ruta): #Si no encuentra el archivo retorna un error
+                    respuesta = "archivo no encontrado"
+                    respuesta.encode('utf-8')
+                with open(ruta,"rb") as file: #Si encuentra el archivo lee su contenido
+                    file.seek(auxseek0)
+                    auxseek0 = auxseek0 + auxseek10
+                    contenido = file.read(auxseek0)
+                    if not contenido:
+                        s.send_multipart([fin])
+                        break
+                    desc_cont = contenido
+                    respuesta="descargado por cliente"
+            elif funcion == "descargarenlace":
+                enlace = aux #Obtiene el enlace de la tercera posicion del comando y la recibe como parametro
+                print(enlace)
+                ruta = enlace.split("$") #Separa en ruta el enlace antes y despues del $
+                print(ruta)
+                n_arch = ruta[0].split("/") #Variable que guarda la dirección del archivo
+                print(n_arch)
+                ruta = "archivos/{}/{}".format(n_arch[1],n_arch[2]) #Se crea la ruta a partir de los parametros anteriores
+                n_archivo = n_arch[2] #El nombre del archivo se encuentra en la tercera posicion, se guarda para enviarlo en el retorno
+                if not path.isfile(ruta):#Si no encuentra el archivo con respecto al enlace retorna un error
+                    respuesta = "archivo no encontrado"
+                    respuesta.encode('utf-8')
+                with open(ruta,"rb") as file: #Si encuentra el archivo lee su contenido
+                    file.seek(auxseek0)
+                    auxseek0 = auxseek0 + auxseek10
+                    contenido = file.read(auxseek0)
+                    if not contenido:
+                        s.send_multipart([fin])
+                        break
+                    desc_cont = contenido
+                    respuesta="descargado por enlace"
+            else : #Condición para función incorrecta
+                respuesta = "error"
+
+            s.send_multipart([respuesta.encode('utf-8'), desc_cont]) #Envia la respuesta (retorno de la función correspondiente) en un json al cliente
